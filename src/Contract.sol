7148e98ad178acdafc790c904fe632e1ef072bd9
sudotx <64108848+sudotx@users.noreply.github.com>
2023-09-22 23:55:34 +0100

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -8,3 +8,21 @@ contract Contract {
         param = param_;
     }
 }
+
+// TODO
+
+// create an erc 4626 vault here
+// learn more about it from saved videos 
+// build it according to the spec lined out in the similar repos you starred
+// add a strategy to earn yield for vault participants
+// allow users flexibility with vault share token? maybe
+
+// build out solid unit, integration tests 
+// ensure test coverage exceeds 80%, as a bare minimum
+// run slither and take the hints it gives to make tests better
+// write deployment scripts, and include tests for that as well
+
+// follow best practices as stated by patrickC 
+// make code as optimized as possible, with detailed comments 
+// take hint for comment structure from other mature codebase(s)
+
