0f198c5946a52b1a7726109527ee5827e983466c
sudotx <64108848+sudotx@users.noreply.github.com>
2023-09-23 14:13:30 +0100

    minor refactor

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,28 +1,52 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Contract {
-    uint256 public immutable param;
+// imports
+import {ERC4626} from "solmate/mixins/ERC4626.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
-    constructor(uint256 param_) {
-        param = param_;
-    }
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+
+contract Vault is ERC4626, ReentrancyGuard {
+    using SafeTransferLib for ERC20;
+
+    // Functions
+    constructor(ERC20 _token, string memory _name, string memory _symbol) ERC4626(_token, _name, _symbol) {}
+    
+    // receive() external payable {}
+
+    // fallback() external payable{}
+
+    // public
+    function totalAssets() public view virtual override returns (uint256) {}
+
+    // internal
+    //  private
+    // internal & private view & pure functions
+    // external & public view & pure functions
 }
 
 // TODO
 
-// create an erc 4626 vault here
-// learn more about it from saved videos 
+// create an erc 4626 vault here ✅
+// use a token depository to select what exact erc20 tokens can be accepted by the vault. or any other standard used in the wild
+// learn more about it from saved videos
 // build it according to the spec lined out in the similar repos you starred
 // add a strategy to earn yield for vault participants
 // allow users flexibility with vault share token? maybe
 
-// build out solid unit, integration tests 
+// build out solid unit, integration tests
 // ensure test coverage exceeds 80%, as a bare minimum
 // run slither and take the hints it gives to make tests better
 // write deployment scripts, and include tests for that as well
 
-// follow best practices as stated by patrickC 
-// make code as optimized as possible, with detailed comments 
+// follow best practices as stated by patrickC
+// make code as optimized as possible, with detailed comments
 // take hint for comment structure from other mature codebase(s)
-
